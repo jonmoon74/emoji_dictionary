c43e355e36a0dbda7ced32e76a28e7930c3cc684
jonmoon74 <jon.moon@outlook.com>
2018-06-03 21:59:56 +0100

    preparing for segue

---
+++ emoji_dictionary/DefinitionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DefinitionViewController.swift
+//  emoji_dictionary
+//
+//  Created by Jon Moon on 03/06/2018.
+//  Copyright © 2018 Jon Moon. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
--- emoji_dictionary/ViewController.swift
+++ emoji_dictionary/ViewController.swift
@@ -40,7 +40,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "moveSegue", sender: "🗜")
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
     }
     
     override func didReceiveMemoryWarning() {
