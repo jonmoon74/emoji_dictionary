fa73395e7b4254d5b179fa2635331e34883351c2
jonmoon74 <jon.moon@outlook.com>
2018-06-04 20:48:09 +0100

    End of initial build of app, with fully functional segue and definitions

--- emoji_dictionary/DefinitionViewController.swift
+++ emoji_dictionary/DefinitionViewController.swift
@@ -12,6 +12,8 @@ class DefinitionViewController: UIViewController {
 
     @IBOutlet weak var emojiLabel: UILabel!
     
+    @IBOutlet weak var definitionLabel: UILabel!
+    
     var emoji = "NO EMOJI"
     
     override func viewDidLoad() {
@@ -20,6 +22,57 @@ class DefinitionViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         emojiLabel.text = emoji
+        
+        if emoji == "😇" {
+            definitionLabel.text = "I am an angel"
+        }
+        if emoji == "💩" {
+            definitionLabel.text = "I am a poo"
+        }
+        if emoji == "💀" {
+            definitionLabel.text = "I am a skull"
+        }
+        if emoji == "👹" {
+            definitionLabel.text = "I am an ogre"
+        }
+        if emoji == "🤬" {
+            definitionLabel.text = "I am a sweary face"
+        }
+        if emoji == "👨‍🍳" {
+            definitionLabel.text = "I am a chef"
+        }
+        if emoji == "🐹" {
+            definitionLabel.text = "I am a cute hamster"
+        }
+        if emoji == "🦑" {
+            definitionLabel.text = "I am a squid"
+        }
+        if emoji == "🥨" {
+            definitionLabel.text = "I am a pretzel"
+        }
+        if emoji == "🎱" {
+            definitionLabel.text = "I am an eightball"
+        }
+        if emoji == "🏕" {
+            definitionLabel.text = "I am tent"
+        }
+        if emoji == "🔐" {
+            definitionLabel.text = "I am a closed lock and key"
+        }
+        if emoji == "👽" {
+            definitionLabel.text = "I am an alien"
+        }
+        if emoji == "😀" {
+            definitionLabel.text = "I am happy"
+        }
+        if emoji == "💒" {
+            definitionLabel.text = "I am a wedding symbol"
+        }
+        if emoji == "🗜" {
+            definitionLabel.text = "I am a clamp"
+        }
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
